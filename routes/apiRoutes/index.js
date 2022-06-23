7c4175bf0b3c565493a2299ceba5407a0a57fc6e
poofy <skyang014@gmail.com>
2022-06-22 21:07:10 -0700

    had to change sql pass

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,11 @@
+//Notice that each route file matches one of the tables, 
+//adhering to separation of concerns.
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
+
+module.exports = router;
